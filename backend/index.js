e7409b199c0497883a8f019f2d6e08caaef9c34e
collo <mutaic84@gmail.com>
2022-10-11 14:36:21 +0300

    implement register functionality'

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import registerService from './services/register';
 import loginService from './services/login';
 import verifyService from './services/verify';
-
+import { buildResponse } from './utils/util';
 
 const healthPath = '/health';
 const registerPath = '/register';
@@ -13,31 +13,22 @@ exports.handler = async (event) => {
    let response;
    switch (true) {
        case event.httpMethod === 'GET' && event.path === healthPath:
-           response = buildResponse(200)
+           response = util.buildResponse(200)
            break;
        case event.httpMethod === 'POST' && event.path === registerPath:
            const registerBody = JSON.parse(event.body);
            response = await registerService.register(registerBody)
            break;
        case event.httpMethod === 'POST' && event.path === loginPath:
-           response = buildResponse(200)
+           response = util.buildResponse(200)
            break;
        case event.httpMethod === 'POST' && event.path === verifyPath:
-           response = buildResponse(200)
+           response = util.buildResponse(200)
            break;
        default:
-           response = buildResponse(404, '404 Not Found')
+           response = util.buildResponse(404, '404 Not Found')
    }
    return response
 };
 
-function buildResponse(statusCode, body) {
-    return {
-        statusCode: statusCode,
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(body)
-    }
-}
+
--- backend/services/register.js
+++ backend/services/register.js
@@ -1,1 +1,78 @@
 import * as aws from 'aws-sdk';
+import util from '../utils/util';
+import bcrypt from 'bcryptjs';
+
+aws.config.update({
+  region: 'ap-northeast-1'
+});
+
+const dynamodb = new aws.DynamoDB.DocumentClient();
+const usersTable = 'test-users';
+
+async function register(userInfo) {
+  const name = userInfo.name;
+  const email = userInfo.email;
+  const username = userInfo.username;
+  const password = userInfo.password;
+
+  if(!username || !name || !email || !password) {
+    return util.buildResponse(401, {
+      message: "All fields are required"
+    })
+  }
+
+  const dynamoUser = await getUser(username);
+  if(dynamoUser && dynamoUser.username){
+    return util.buildResponse(401, {
+      message: "Username already exists in our database. Please choose a different username"
+    })
+  }
+
+  const encryptedPassword = bcrypt.hashSync(password.trim(), 10);
+  const user = {
+    name: name,
+    email: email,
+    username: username.toLowerCase().trim(),
+    password: encryptedPassword
+  }
+
+  const saveUserResponse = await saveUser(user);
+
+  if(!saveUserResponse) {
+    return util.buildResponse(503, {
+      message: "Server error! Please try again later"
+    })
+  }
+
+  return util.buildResponse(200, {
+    username: username
+  })
+}
+
+async function getUser(username) {
+  const params = {
+    TableName: usersTable,
+    Key: {
+      username: username
+    }
+  }
+
+  return await dynamodb.get(params).promise().then(response => {
+    return response.Item;
+  }, error => {
+    console.log("There was an error getting user: ", error)
+  })
+}
+
+async function saveUser(user) {
+  const params = {
+    TableName: usersTable,
+    Item: user
+  }
+
+  return await dynamodb.put(params).promise().then(() => {
+    return true;
+  }, error => console.log("An error occurred saving user: ", error))
+}
+
+module.exports.register = register
